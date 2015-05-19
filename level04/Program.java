0622ef26b6ad18f6460a4aa2c0c75c51f708846d
Akshay Joshi <a24joshi@uwaterloo.ca>
2015-05-19 17:52:56 -0400

    Add code for level04.

---
+++ level04/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+  public static void main() {
+    System.out.println("Can you see this? You're doing quite well so far.");
+    System.out.println("Here is the code you seek: NKJciL4nuUsW8Q7IiCnp");
+    System.out.println("Well done, young grasshopper.");
+  }
+}
