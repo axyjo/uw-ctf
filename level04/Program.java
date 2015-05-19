5f2b25fc83b8d174419df9f02f1a1d09b7539024
Akshay Joshi <me@akshayjoshi.com>
2015-05-19 18:02:58 -0400

    Update level04 code.

--- level04/Program.java
+++ level04/Program.java
@@ -1,5 +1,5 @@
 public class Program {
-  public static void main() {
+  public static void main(String[] args) {
     System.out.println("Can you see this? You're doing quite well so far.");
     System.out.println("Here is the code you seek: NKJciL4nuUsW8Q7IiCnp");
     System.out.println("Well done, young grasshopper.");
